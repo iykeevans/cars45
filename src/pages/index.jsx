4b36c962afcc6d487f71cb766149848266a07d6d
l.olanipekun@yahoo.com <l.olanipekun@yahoo.com>
2021-04-05 01:04:54 +0100

    done with selected part

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,7 +21,7 @@ const Home = (props) => {
   }, []);
 
   const [loading, setLoading] = useState(false);
-  
+
   const responsive = {
     0: {
       items: 1,
@@ -65,7 +65,7 @@ const Home = (props) => {
     },
   };
 
- 
+
   return (
     <HomeLayout>
       {loading && <Loading />}
@@ -105,36 +105,36 @@ const Home = (props) => {
                 <div className="row">
                   <div className="col-10  col-md-6 mx-auto">
                     <h1>
-                      Selling, buying and swapping cars has never been this easy
+                      {/* Selling, buying and swapping cars has never been this easy */}
                     </h1>
                   </div>
                 </div>
               </div>
-              <img src="/assets/images/banner.svg" alt="banner" />
+              <img src="/assets/images/banner.png" alt="banner" />
             </div>
             <div className="item">
               <div className="banner-text">
                 <div className="row">
                   <div className="col-10  col-md-6 mx-auto">
                     <h1>
-                      Selling, buying and swapping cars has never been this easy
+                      {/* Selling, buying and swapping cars has never been this easy */}
                     </h1>
                   </div>
                 </div>
               </div>
-              <img src="/assets/images/banner.svg" alt="banner" />
+              <img src="/assets/images/banner3.png" alt="banner" />
             </div>
             <div className="item">
               <div className="banner-text">
                 <div className="row">
                   <div className="col-10  col-md-6 mx-auto">
                     <h1>
-                      Selling, buying and swapping cars has never been this easy
+                      {/* Selling, buying and swapping cars has never been this easy */}
                     </h1>
                   </div>
                 </div>
               </div>
-              <img src="/assets/images/banner.svg" alt="banner" />
+              <img src="/assets/images/banner4.png" alt="banner" />
             </div>
           </OwlCarousel>
           <div className="row banner-bottom">
@@ -152,8 +152,8 @@ const Home = (props) => {
             Feedback
           </button>
         </div>
-        <Search/>
-<FeaturedCar/>
+        <Search />
+        <FeaturedCar />
 
         <div className="section4">
           <div className="container">
@@ -217,17 +217,118 @@ const Home = (props) => {
                       <div className="col-md-3">
                         <div className="img-cont">
                           <div className="img">
-                            <img src="/assets/images/user.svg" alt="user" />
+                            <img src="/assets/images/Adeoya-Temitope-Earned-50k.png" alt="user" />
                           </div>
                         </div>
                       </div>
 
                       <div className="col-md-9 user-details text-center">
-                        <p className="name">Adebayo Oyewole</p>
+                        <p className="name">Adeoya Temitope</p>
                         <img src="/assets/images/quote.svg" alt="..." />
                         <p className="detail">
-                          Excellent Customer service. I had a great experience
-                          selling my car on Cars45
+                          I earned a commision of 50k on cars45
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="item">
+                    <div className="row">
+                      <div className="col-md-3">
+                        <div className="img-cont">
+                          <div className="img">
+                            <img src="/assets/images/AKINSOTO-OLUWATOYOSI-COMMISSION-EARNED-50K.jpeg" alt="user" />
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="col-md-9 user-details text-center">
+                        <p className="name">Akinsoto Oluwatoyosi</p>
+                        <img src="/assets/images/quote.svg" alt="..." />
+                        <p className="detail">
+                          I earned a commision of 50k on cars45
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="item">
+                    <div className="row">
+                      <div className="col-md-3">
+                        <div className="img-cont">
+                          <div className="img">
+                            <img src="/assets/images/CHIAWA-KELLY-AMECHI-COMMISSION-EARNED-50K.jpeg" alt="user" />
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="col-md-9 user-details text-center">
+                        <p className="name">Chiawa Kelly</p>
+                        <img src="/assets/images/quote.svg" alt="..." />
+                        <p className="detail">
+                          I earned a commision of 50k on cars45
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="item">
+                    <div className="row">
+                      <div className="col-md-3">
+                        <div className="img-cont">
+                          <div className="img">
+                            <img src="/assets/images/EGBEOLA-PAUL-COMMISSION-EARNED-100K.jpeg" alt="user" />
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="col-md-9 user-details text-center">
+                        <p className="name">Egbeola Paul</p>
+                        <img src="/assets/images/quote.svg" alt="..." />
+                        <p className="detail">
+                          I earned a commision of 100k on cars45
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+
+                  <div className="item">
+                    <div className="row">
+                      <div className="col-md-3">
+                        <div className="img-cont">
+                          <div className="img">
+                            <img src="/assets/images/James-John-Adoyi-Commission-Earned-80k.jpeg" alt="user" />
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="col-md-9 user-details text-center">
+                        <p className="name">James John</p>
+                        <img src="/assets/images/quote.svg" alt="..." />
+                        <p className="detail">
+                          I earned a commision of 80k on cars45
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+
+                  <div className="item">
+                    <div className="row">
+                      <div className="col-md-3">
+                        <div className="img-cont">
+                          <div className="img">
+                            <img src="/assets/images/NDU-COLLINS-CHIMAOBI-COMMISSION-EARNED-80K.jpeg" alt="user" />
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="col-md-9 user-details text-center">
+                        <p className="name">Ndu Collins</p>
+                        <img src="/assets/images/quote.svg" alt="..." />
+                        <p className="detail">
+                          I earned a commision of 80k on cars45
                         </p>
                       </div>
                     </div>
